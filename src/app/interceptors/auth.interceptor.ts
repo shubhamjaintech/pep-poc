60988ce6cbd50e4a671f01c93eea210b1e963092
Shubham Jain <shubham.jain@encora.com>
2025-09-15 04:37:26 +0530

    fix: typos fixed

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -1,11 +1,11 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { Router } from '@angular/router';
+// import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
-  const router = inject(Router);
+  // const router = inject(Router);
   
   const token = authService.getToken();
   if (token && !req.url.includes('/login')) {
